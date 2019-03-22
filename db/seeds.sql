44c13c0931dd8660f9b7f6d57d7e8c465342529f
ryan.w.daly <ryan.w.daly90@gmail.com>
2019-03-22 15:00:05 -0400

    did I commit already? idk

--- db/seeds.sql
+++ db/seeds.sql
@@ -3,15 +3,15 @@ INSERT INTO burgers_db (
     devoured
 )
   
-INSERT INTO burgers (burger_name, devoured) VALUES("Batman", 1);
-INSERT INTO burgers (burger_name, devoured) VALUES("Lexi", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES("Batman", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES("Lexi", 0);
 INSERT INTO burgers (burger_name, devoured) VALUES("Swiss Shroom", 0);
-INSERT INTO burgers (burger_name, devoured) VALUES("Diablo", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES("Diablo", 0);
 INSERT INTO burgers (burger_name, devoured) VALUES("Plain", 0);
 INSERT INTO burgers (burger_name, devoured) VALUES("Juniors (Beef Only)", 0);
-INSERT INTO burgers (burger_name, devoured) VALUES("Mule", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES("Mule", 0);
 INSERT INTO burgers (burger_name, devoured) VALUES("California", 0);
-INSERT INTO burgers (burger_name, devoured) VALUES("Rocky", 1);
-INSERT INTO burgers (burger_name, devoured) VALUES("Texan", 1);
-INSERT INTO burgers (burger_name, devoured) VALUES("Dare Devil", 1);
+INSERT INTO burgers (burger_name, devoured) VALUES("Rocky", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES("Texan", 0);
+INSERT INTO burgers (burger_name, devoured) VALUES("Dare Devil", 0);
 
