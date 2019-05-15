cb51a12da0ac9179f5cfae9b683c35a4cdb5734e
ryan.w.daly <ryan.w.daly90@gmail.com>
2019-05-15 18:31:47 -0400

    working locally

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 var exphbs = require("express-handlebars");
 var app = express();
-require('dotenv').config()
+// require('dotenv').config()
 var PORT = process.env.PORT || 8080;
 // Serve static content for the app from the "public" directory in the application directory.
 app.use(express.static(process.cwd() + "/public"));
